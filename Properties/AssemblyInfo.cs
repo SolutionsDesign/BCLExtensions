6ff2efad52888137053474d11d326701ccac8eda
Solutionsdesign <none@none>
2010-11-27 12:44:05 +0100

    Initial commit of svn export.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SD.Tools.BCLExtensions")]
+[assembly: AssemblyDescription("Extension methods for the .NET Base Class Library classes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Solutions Design bv")]
+[assembly: AssemblyProduct("BCLExtensions")]
+[assembly: AssemblyCopyright("Copyright © Solutions Design 2010")]
+[assembly: AssemblyTrademark(".NET is a trademark of Microsoft Corp.")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bcab090a-ca43-4a6e-8789-063f61f7e3fd")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.10.0907")]
