7e3c960dc674aee9176c6140b27327b68d7668ba
Frans Bouma <frans@sd.nl>
2016-06-30 13:12:39 +0200

    Move to src folder, .NET core support

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 BCLExtensions
 =============
 
-BCLExtensions is an extension method library for .NET 3.5 or higher which offers various extension methods for classes in the .NET Base Class Library (BCL)
+BCLExtensions is an extension method library for .NET 3.5 or higher or for .NET Core which offers various extension methods for classes in the .NET Base Class Library (BCL)
 
-BCL Extensions is a small library which extends what is shipped with .NET 3.5 or .NET 4.0. Please consult the [Documentation](https://github.com/SolutionsDesign/BCLExtensions/wiki) for further details. BCL Extensions is used by [LLBLGen Pro v3 and up](http://www.llblgen.com/) and the algorithm and data-structures library [Algorithmia](https://github.com/SolutionsDesign/Algorithmia) and has been used in production successfully since May 2010.
+BCL Extensions is a small library which extends what is shipped with .NET 3.5+ / .NET Core. Please consult the [Documentation](https://github.com/SolutionsDesign/BCLExtensions/wiki) for further details. BCL Extensions is used by [LLBLGen Pro v3 and up](http://www.llblgen.com/) and the algorithm and data-structures library [Algorithmia](https://github.com/SolutionsDesign/Algorithmia) and has been used in production successfully since May 2010.
+
+### .NET Core support
+All functionality is there, except the DataSet/Table extension methods as .NET Core doesn't support these types. 
 
 ### Getting a binary version via NuGet
 
 BCL Extensions is also [available on Nuget](https://nuget.org/packages/SD.Tools.BCLExtensions/). 
 
 ### License
-BCL Extensions is &copy; 2010-2013 [Solutions Design bv](http://www.sd.nl/) and is released under the [BSD2 license](https://github.com/SolutionsDesign/BCLExtensions/blob/master/LICENSE.txt).
+BCL Extensions is &copy; 2010-2016 [Solutions Design bv](http://www.sd.nl/) and is released under the [BSD2 license](https://github.com/SolutionsDesign/BCLExtensions/blob/master/LICENSE.txt).
---
+++ src/DataRelated/DataSetExtensionMethods.cs
@@ -0,0 +1,88 @@
+﻿#if (NET35)
+//////////////////////////////////////////////////////////////////////
+// BCLExtensions is (c) 2010 Solutions Design. All rights reserved.
+// http://www.sd.nl
+//////////////////////////////////////////////////////////////////////
+// COPYRIGHTS:
+// Copyright (c) 2010 Solutions Design. All rights reserved.
+// 
+// The BCLExtensions library sourcecode and its accompanying tools, tests and support code
+// are released under the following license: (BSD2)
+// ----------------------------------------------------------------------
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met: 
+//
+// 1) Redistributions of source code must retain the above copyright notice, this list of 
+//    conditions and the following disclaimer. 
+// 2) Redistributions in binary form must reproduce the above copyright notice, this list of 
+//    conditions and the following disclaimer in the documentation and/or other materials 
+//    provided with the distribution. 
+// 
+// THIS SOFTWARE IS PROVIDED BY SOLUTIONS DESIGN ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SOLUTIONS DESIGN OR CONTRIBUTORS BE LIABLE FOR 
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+//
+// The views and conclusions contained in the software and documentation are those of the authors 
+// and should not be interpreted as representing official policies, either expressed or implied, 
+// of Solutions Design. 
+//
+//////////////////////////////////////////////////////////////////////
+// Contributers to the code:
+//		- Frans Bouma [FB]
+//////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using SD.Tools.BCLExtensions.SystemRelated;
+
+namespace SD.Tools.BCLExtensions.DataRelated
+{
+	/// <summary>
+	/// Class for DataSet related extension methods.
+	/// </summary>
+	public static class DataSetExtensionMethods
+	{
+		/// <summary>
+		/// Gets the value of the column with the column name specified from the DataRow in the type specified. If the value is DBNull.Value, null / Nothing
+		/// will be returned, if TValue is a nullable value type or a reference type, the default value for TValue will be returned otherwise. 
+		/// </summary>
+		/// <typeparam name="TValue">The type of the value.</typeparam>
+		/// <param name="row">The row.</param>
+		/// <param name="columnName">Name of the column.</param>
+		/// <returns>the value of the column specified, or the default value for the type specified if not found.</returns>
+		/// <remarks>Use this method instead of Field(Of TValue) if you don't want to receive cast exceptions</remarks>
+		public static TValue Value<TValue>(this DataRow row, string columnName)
+		{
+			TValue toReturn = default(TValue);
+			if(!((row == null) || string.IsNullOrEmpty(columnName) || (row.Table == null) || !row.Table.Columns.Contains(columnName)))
+			{
+				object columnValue = row[columnName];
+				if(columnValue != DBNull.Value)
+				{
+					Type destinationType = typeof(TValue);
+					if(typeof(TValue).IsNullableValueType())
+					{
+						destinationType = destinationType.GetGenericArguments()[0];
+					}
+					if(columnValue is TValue)
+					{
+						toReturn = (TValue)columnValue;
+					}
+					else
+					{
+						toReturn = (TValue)Convert.ChangeType(columnValue, destinationType);
+					}
+				}
+			}
+			return toReturn;
+		}
+	}
+}
+#endif
