5abbe529d1f666172d433cc1e795f632b532769c
Frans Bouma <frans@sd.nl>
2016-07-01 15:43:18 +0200

    Changes:
    - Removed .NET core test project. Simply doesn't work at the moment with
    the tooling in pre-alpha state
    - Moves to a conventional .NET 4.6.1 test project. Will add tests to
    this project, which tests the .NET full code. No testing on .net core
    this way :(
    - Cleaned up some crap in the root folder
    - Added strong key to build.

---
+++ test/DictionaryExtensionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+using SD.Tools.BCLExtensions.CollectionsRelated;
+
+namespace Tests
+{
+	[TestFixture]
+    public class DictionaryExtensionTests
+    {
+		[Test]
+		public void AddRangeTest()
+		{
+			var toAdd = new List<int> {1, 2, 3, 4, 5};
+			var toAddTo = new Dictionary<string, int>();
+			Assert.AreEqual(0, toAddTo.Count);
+			toAddTo.AddRange(i=>i.ToString(), toAdd);
+			Assert.IsTrue(toAddTo.Any());
+			Assert.AreEqual(toAdd.Count, toAddTo.Count);
+			foreach(var kvp in toAddTo)
+			{
+				Assert.AreEqual(kvp.Key, kvp.Value.ToString());
+				Assert.IsTrue(toAdd.Contains(kvp.Value));
+			}
+		}
+    }
+}
--- test/Properties/AssemblyInfo.cs
+++ test/Properties/AssemblyInfo.cs
@@ -2,18 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tester")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0dfc1960-c458-40da-98ca-c0e460c0845e")]
+[assembly: Guid("4cafe265-7b58-461a-a899-704e9c065499")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
