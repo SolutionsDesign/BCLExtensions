fbe9718fe3f67d2b455dc5fed1308c2b10fe7e36
Frans Bouma <frans@sd.nl>
2016-07-01 15:03:16 +0200

    Trying to add test project. Failed.

---
+++ test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SD.Tools.BCLExtensions;
+
+namespace Tester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+
+	[TestFixture]
+	public class DictionaryTests
+	{
+		[Test]
+		public void AddRangeTest()
+		{
+			var toAdd = new List<int> {1, 2, 3, 4, 5};
+			var toAddTo = new Dictionary<string, int>();
+			toAddTo.AddRange
+		}
+	}
+
+}
